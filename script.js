85ce6ca26cd8c4da822ae2a9dc74d38f667d0296
Abhishek Kumar Mehta <93569162+Abhi865625@users.noreply.github.com>
2024-01-01 00:09:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,70 @@
 const quoteText = document.querySelector(".quote"),
-quoteBtn = document.querySelector("button"),
-authorName = document.querySelector(".name"),
-speechBtn = document.querySelector(".speech"),
-copyBtn = document.querySelector(".copy"),
-twitterBtn = document.querySelector(".twitter"),
-synth = speechSynthesis;
+  quoteBtn = document.querySelector("button"),
+  authorName = document.querySelector(".name"),
+  speechBtn = document.querySelector(".speech"),
+  copyBtn = document.querySelector(".copy"),
+  twitterBtn = document.querySelector(".twitter"),
+  synth = speechSynthesis;
 
-function randomQuote(){
-    quoteBtn.classList.add("loading");
-    quoteBtn.innerText = "Loading Quote...";
-    fetch("http://api.quotable.io/random").then(response => response.json()).then(result => {
-        quoteText.innerText = result.content;
-        authorName.innerText = result.author;
-        quoteBtn.classList.remove("loading");
-        quoteBtn.innerText = "New Quote";
+function randomQuote() {
+  quoteBtn.classList.add("loading");
+  quoteBtn.innerText = "Loading Quote...";
+  fetch("http://api.quotable.io/random")
+    .then((response) => response.json())
+    .then((result) => {
+      quoteText.innerText = result.content;
+      authorName.innerText = result.author;
+      quoteBtn.classList.remove("loading");
+      quoteBtn.innerText = "New Quote";
     });
 }
 
-speechBtn.addEventListener("click", ()=>{
-    if(!quoteBtn.classList.contains("loading")){
-        let utterance = new SpeechSynthesisUtterance(`${quoteText.innerText} by ${authorName.innerText}`);
-        synth.speak(utterance);
-        setInterval(()=>{
-            !synth.speaking ? speechBtn.classList.remove("active") : speechBtn.classList.add("active");
-        }, 10);
-    }
+speechBtn.addEventListener("click", () => {
+  if (!quoteBtn.classList.contains("loading")) {
+    let utterance = new SpeechSynthesisUtterance(
+      `${quoteText.innerText} by ${authorName.innerText}`
+    );
+    synth.speak(utterance);
+    setInterval(() => {
+      !synth.speaking
+        ? speechBtn.classList.remove("active")
+        : speechBtn.classList.add("active");
+    }, 10);
+  }
 });
 
-copyBtn.addEventListener("click", ()=>{
-    navigator.clipboard.writeText(quoteText.innerText);
+copyBtn.addEventListener("click", () => {
+  navigator.clipboard.writeText(quoteText.innerText);
 });
 
-twitterBtn.addEventListener("click", ()=>{
-    let tweetUrl = `https://twitter.com/intent/tweet?url=${quoteText.innerText}`;
-    window.open(tweetUrl, "_blank");
+twitterBtn.addEventListener("click", () => {
+  let tweetUrl = `https://twitter.com/intent/tweet?url=${quoteText.innerText}`;
+  window.open(tweetUrl, "_blank");
 });
 
 quoteBtn.addEventListener("click", randomQuote);
+
+function showCopiedMessage() {
+  const copiedMessage = document.createElement("div");
+  copiedMessage.textContent = "Quote copied!";
+  copiedMessage.classList.add("copied-message");
+
+  // Insert message before the copy button
+  copyBtn.parentNode.insertBefore(copiedMessage, copyBtn);
+
+  // Remove the message after 2 seconds
+  setTimeout(() => {
+    copiedMessage.remove();
+  }, 2000);
+}
+
+copyBtn.addEventListener("click", () => {
+  navigator.clipboard
+    .writeText(quoteText.innerText)
+    .then(() => {
+      showCopiedMessage();
+    })
+    .catch((err) => {
+      console.error("Could not copy text: ", err);
+    });
+});
